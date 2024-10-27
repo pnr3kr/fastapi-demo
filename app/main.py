26f81984622cacc383ffff80a27e45819e4e58f5
Tristen Davin <pnr3kr@virginia.edu>
2024-10-27 17:17:26 -0400

    yaml file

--- app/main.py
+++ app/main.py
@@ -19,3 +19,8 @@ def add(a: int, b: int):
 @app.get("/multiply/{c}/{d}")
 def multiply(c: int, d: int):
     return {"product": c * d}
+
+@app.get("/square/{a}")
+async def square(a: int):
+    return {"square": a * a}
+
